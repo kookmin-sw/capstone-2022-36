abef88c9b1952926040cc775058273f08930dc48
topcue <topcue1019@naver.com>
2022-05-22 20:22:11 +0900

    Add source code samples

---
+++ samples/java_v2.java
@@ -0,0 +1,19 @@
+public static ImageIcon getImageIcon(String path)
+{
+  if (path == null)
+  {
+    log.error("Icon path is null");
+    return null;
+  }
+
+  java.net.URL imgURL = GuiImporter.class.getResource(path);
+
+  if (imgURL == null)
+  {
+    log.error("Couldn't find icon: " + imgURL);
+    return null;
+  }
+  else
+    return new ImageIcon(imgURL);
+
+}
