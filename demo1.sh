7d9d4d8058029ef8363ed70c5efe14ccc37f88e2
topcue <topcue1019@naver.com>
2022-05-22 20:21:45 +0900

    Add demo scripts

---
+++ demo1.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+foo() {
+  echo -e "${GREEN}[*] $1${NOCOLOR}"
+  echo -e "${GREEN}    (Press any key to continue...)${NOCOLOR}"
+  arg=$1
+  read
+  eval ${arg}
+}
+
+
+alias cat=bat
+foo "cat samples/java_v1.java"
+foo "cat samples/java_v2.java"
+foo "dotnet run --no-build --project ~/capstone-2022-36/B2R2/src/RearEnd/BinDump -- samples/java_v1.java samples/java_v2.java --diff --text --show-color"
+
+
+
+
+# EOF
---
+++ demo2.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+foo() {
+  echo -e "${GREEN}[*] $1${NOCOLOR}"
+  echo -e "${GREEN}    (Press any key to continue...)${NOCOLOR}"
+  arg=$1
+  read
+  eval ${arg}
+}
+
+
+foo "file samples/a.out"
+foo "file samples/b.out"
+foo "dotnet run --no-build --project ~/capstone-2022-36/B2R2/src/RearEnd/BinDump -- samples/a.out samples/b.out --diff --show-color --myers"
+
+
+
+# EOF
