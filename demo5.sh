5240e337749f2e5b143a23aac523b02ceb923c26
topcue <topcue1019@naver.com>
2022-05-23 13:27:49 +0900

    Add demo scripts

---
+++ demo5.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+foo() {
+  echo -e "${GREEN}[*] $1${NOCOLOR}"
+  echo -e "${GREEN}    (Press any key to continue...)${NOCOLOR}"
+  arg=$1
+  read
+  eval ${arg}
+}
+
+
+foo "cat samples/paths.txt"
+foo "dotnet run --no-build --project ~/capstone-2022-36/B2R2/src/RearEnd/BinDump -- samples/paths.txt --multiple"
+
+
+
+# EOF
